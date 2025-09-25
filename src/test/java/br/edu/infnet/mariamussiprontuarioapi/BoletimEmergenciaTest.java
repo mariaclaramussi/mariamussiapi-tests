a35845b4a94f15dd7d41e7e8bc4d878f4d991fd1
Maria Clara Mussi <mariamussis@outlook.com>
2025-09-25 16:47:43 -0300

    Red Phase: failed testes for BoletimEmergenciaTest

---
+++ src/test/java/br/edu/infnet/mariamussiprontuarioapi/BoletimEmergenciaTest.java
@@ -0,0 +1,30 @@
+package br.edu.infnet.mariamussiprontuarioapi;
+
+import org.junit.jupiter.api.Test;
+
+public class BoletimEmergenciaTest {
+
+    @Test
+    void construtorDeveInicializarComClassificacaoBaixaEDataEntradaAtual() {}
+
+    @Test
+    void devePermitirDefinirEObterNumeroProntuario() { }
+
+    @Test
+    void devePermitirDefinirEObterPlanoSaude(){}
+
+    @Test
+    void devePermitirDefinirEObterPaciente() {}
+
+    @Test
+    void devePermitirDefinirEObterAnamese() {}
+
+    @Test
+    void devePermitirDefinirEObterClassificacaoRisco() {}
+
+    @Test
+    void devePermitirDefinirEObterDataSaida() {}
+
+    @Test
+    void devePermitirDefinirEObterAgendamentos() {}
+}
