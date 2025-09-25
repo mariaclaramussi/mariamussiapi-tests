ef488041d46b1ad209fea22bb72dfd8aa1d8f9a6
Maria Clara Mussi <mariamussis@outlook.com>
2025-09-25 16:58:42 -0300

    Red Phase: failed tests for Agendamento class

---
+++ src/test/java/br/edu/infnet/mariamussiprontuarioapi/AgendamentoTest.java
@@ -0,0 +1,41 @@
+package br.edu.infnet.mariamussiprontuarioapi;
+
+import org.junit.jupiter.api.Test;
+
+public class AgendamentoTest {
+        @Test
+        void devePermitirDefinirEObterBoletim() {
+
+        }
+
+        @Test
+        void devePermitirDefinirEObterPlanoDeSaude() {
+
+        }
+
+        @Test
+        void devePermitirDefinirEObterTipoConsulta() {
+
+        }
+
+        @Test
+        void devePermitirDefinirEObterValor() {
+
+        }
+
+        @Test
+        void devePermitirDefinirEObterData() {
+
+        }
+
+        @Test
+        void devePermitirDefinirEObterPaciente() {
+
+        }
+
+        @Test
+        void devePermitirDefinirEObterMedico() {
+
+        }
+}
+
